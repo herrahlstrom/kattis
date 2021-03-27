a69f67a19d47015ee933ca4f9ea74a79cd57e6fe
Martin Ahlström <martin.ahlstrom@outlook.com>
2021-03-27 13:18:01 +0100

    Skriver om Teque, fortfarande för långsam :-(

--- teque/teque.cs
+++ teque/teque.cs
@@ -9,65 +9,109 @@ namespace Teque
 {
     class Teque<T>
     {
-        List<T> _q = new List<T>();
+        private Dictionary<int, T> _front = new Dictionary<int, T>();
+        private Dictionary<int, T> _back = new Dictionary<int, T>();
+        private int _frontHead = -1;
+        private int _frontTail = 0;
+        private int _backHead = -1;
+        private int _backTail = 0;
 
+        private void ReArrange()
+        {
+            if (_front.Count < _back.Count)
+            {
+                _front[_frontTail++] = _back[_backHead + 1];
+                _back.Remove(++_backHead);
+            }
+            else if (_front.Count - 1 > _back.Count)
+            {
+                _back[_backHead--] = _front[_frontTail - 1];
+                _front.Remove(--_frontTail);
+            }
+        }
         public void PushBack(T value)
         {
-            _q.Add(value);
+            _back[_backTail++] = value;
+            ReArrange();
         }
 
         public void PushMiddle(T value)
         {
-            int position = (_q.Count + 1) / 2;
-            _q.Insert(position, value);
+            _front[_frontTail++] = value;
+            ReArrange();
         }
 
         public void PushFront(T value)
         {
-            _q.Insert(0, value);
+            _front[_frontHead--] = value;
+            ReArrange();
         }
 
         public T Get(int position)
         {
-            return _q[position];
+            return position < _front.Count
+                ? _front[_frontHead + position + 1]
+                : _back[position - _front.Count + _backHead + 1];
         }
     }
-    
+
     class Program
     {
+        private static Teque<long> _q = new Teque<long>();
+        private static void Command(string line)
+        {
+            var lineArr = line.Split(' ');
+            switch (lineArr[0])
+            {
+                case "push_back":
+                    _q.PushBack(long.Parse(lineArr[1]));
+                    break;
+                case "push_front":
+                    _q.PushFront(long.Parse(lineArr[1]));
+                    break;
+                case "push_middle":
+                    _q.PushMiddle(long.Parse(lineArr[1]));
+                    break;
+                case "get":
+                    Console.WriteLine(_q.Get(int.Parse(lineArr[1])));
+                    break;
+                default:
+                    throw new ArgumentException("Invalid line: " + line);
+            }
+        }
         static void Main(string[] args)
         {
-            var q = new Teque<long>();
+            //var sw = System.Diagnostics.Stopwatch.StartNew();
+
+
 
             int noOps = int.Parse(Console.ReadLine());
-            while(noOps>0) {
-                noOps--;            
-                var line = Console.ReadLine();
-                if(line[0] == 'g') 
+
+            var input = new Queue<string>();
+
+            var buffer = new char[1024];
+            bool readDone = false;
+            do
+            {
+                var readed = Console.In.Read(buffer, 0, buffer.Length);
+                if (readed < buffer.Length)
                 {
-                    int position = int.Parse(line.Substring(4));
-                    Console.WriteLine(q.Get(position));
-                } 
-                else if(line[5] == 'b') 
-                {
-                    long value = long.Parse(line.Substring(10));
-                    q.PushBack(value);
-                } 
-                else if(line[5] == 'f') 
-                {
-                    long value = long.Parse(line.Substring(11));
-                    q.PushFront(value);                    
-                } 
-                else if(line[5] == 'm') 
-                {
-                    long value = long.Parse(line.Substring(12));
-                    q.PushMiddle(value);                    
-                } 
-                else 
-                {
-                    throw new ArgumentException("Invalid line: " + line);
+                    readDone = true;
                 }
+
+                var r = new System.IO.StringReader()
+
+            } while (!readDone);
+
+            while (noOps > 0)
+            {
+                noOps--;
+                string line = Console.ReadLine();
+                Command(line);
             }
+
+            // sw.Stop();
+            // Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms.");
         }
     }
 }
