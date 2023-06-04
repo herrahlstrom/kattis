5638c95413ed8336ae40d1b6802c6b01a7ac8832
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-06-04 20:23:21 +0200

    Build zones instead of find path for all queries

--- 10kindsofpeople/10kindsofpeople.py
+++ 10kindsofpeople/10kindsofpeople.py
@@ -4,52 +4,46 @@ https://open.kattis.com/problems/10kindsofpeople
 
 from typing import List
 
-RESULT_NEITHER = "neither"
-RESULT_DECIMAL = "decimal"
-RESULT_BINARY = "binary"
-
 rows: int
 columns: int
 map = []
-queries: List[tuple] = []
 
 
 def getNeighbours(index: int) -> List[int]:
-    global columns
-    yield index - columns
+    global columns, map
+    if index >= columns:
+        yield index - columns
     if index % columns > 0:
         yield index - 1
     if index % columns < (columns-1):
         yield index + 1
-    yield index + columns
+    if(index + columns < len(map)):
+        yield index + columns
 
 
-def getValidNeighbours(index: int) -> List[int]:
+def build_zones():
     global map
-    for n in getNeighbours(index):
-        if n >= 0 and n < len(map) and map[n] == map[index]:
-            yield n
-
-def solve(map, start: int, end: int):
-    if start == end:
-        return RESULT_BINARY if map[start] == "0" else RESULT_DECIMAL
-    if map[start] != map[end]:
-        return RESULT_NEITHER
-    
-    visited = [False] * len(map)
-    q = [start]
-    while len(q) > 0:
-        current = q.pop()
-        if visited[current]:
+    zone_queue = []
+    next_even = 2
+    next_odd = 3
+    for i in range(len(map)):
+        if map[i] > 1:
             continue
-        visited[current] = True
-        for neighbour in getValidNeighbours(current):
-            if neighbour == end:
-                return RESULT_BINARY if map[start] == "0" else RESULT_DECIMAL
-            if not visited[neighbour]:
-                q.append(neighbour)
-            
-    return RESULT_NEITHER
+        current_value = map[i]
+        zone_value:int
+        if map[i] % 2 == 0:
+            zone_value = next_even
+            next_even += 2
+        else:
+            zone_value = next_odd
+            next_odd += 2
+        zone_queue.append(i)
+        while len(zone_queue) > 0:
+            current = zone_queue.pop()
+            map[current] = zone_value
+            for neighbour in getNeighbours(current):
+                if map[neighbour] == current_value:
+                    zone_queue.append(neighbour)
 
 
 # Dimension
@@ -57,16 +51,19 @@ rows, columns = [int(x) for x in input().split()]
 
 # Map
 for x in range(rows):
-    map.extend(list(input()))
+    map.extend([int(x) for x in input()])
+
+build_zones()
 
 # Queries
-num_queries = int(input())
-for i in range(num_queries):
+for i in range(int(input())):
     y1, x1, y2, x2 = [int(x)-1 for x in input().split()]
     start = y1 * columns + x1
     end = y2 * columns + x2
-    queries.append((start, end))
-
-
-for query in queries:
-    print(solve(map, query[0], query[1]))
+    if map[start] == map[end]:
+        if map[start] % 2 == 0:
+            print("binary")
+        else:
+            print("decimal")
+    else:
+        print("neither")
